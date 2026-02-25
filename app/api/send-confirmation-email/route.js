6243d1cb14885cafc53c85bc2afe0ce808c26995
cildenis <cil.deniz@icloud.com>
2026-02-25 10:45:04 +0900

    Update Resend API key environment variable

--- app/api/send-confirmation-email/route.js
+++ app/api/send-confirmation-email/route.js
@@ -3,7 +3,7 @@ import { Resend } from 'resend';
 
 export const dynamic = 'force-dynamic'; 
 
-const resend = new Resend(process.env.RESEND_API_KEY); 
+const resend = new Resend(process.env.NEXT_PUBLIC_RESEND_API_KEY); 
 
 export async function POST(request) {
   try {
